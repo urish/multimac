9fee68bfb6d7ed773df4d9a4a1c476b0be4357bb
Jonny Edwards <jonny@temporalcomputing.com>
2024-02-28 10:16:45 +0000

    initial changes

--- src/project.v
+++ src/project.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Jonny Edwards
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `define default_netname none
 
-module tt_um_example (
+module tt_um_fountaincoder_top (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,9 +16,18 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+DMADD madd(
+	  	.clk    (clk),
+	  	.ena 	(ena),
+	  	.run    (ui_in[3]),
+	  	.load   (ui_in[2]),
+	  	.insn   (ui_in[1:0]),
+		.index  (uio_in[7:4]),
+		.data   (uio_in[3:0]),
+		.out    (uo_out),
+		.out_i  (uo_out2),
+		.rst_n  (rst_n)
+
+);
 
 endmodule
