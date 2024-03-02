04b7e7e0ebe105da460794dac999127255ca74d9
Jonny Edwards <jonny@temporalcomputing.com>
2024-03-02 10:30:18 +0000

    more work on getting it working

--- docs/info.md
+++ docs/info.md
@@ -1,20 +1,19 @@
-<!---
-
-This file is used to generate your project datasheet. Please fill in the information below and delete any unused
-sections.
-
-You can also include images in this folder and reference them in the markdown. Each image must be less than
-512 kb in size, and the combined size of all images must be less than 1 MB.
--->
 
 ## How it works
 
-Explain how your project works
+This is a simple circuit to calculate: 
+- a vector dot product ie the sum of `w_i*x_i` where `i` can be anything up to about 40 (`insn=2`)
+- Minimum of a list of data (`insn=0`)
+- Maximum of a list of data (`insn=1`)
+
+It has been designed as a coprocessor. The data is first added by setting `load=1` and then supplying the data
+for the dot product the `index` and `data`. Each set is a `w`,`x` pair. Its a 4 bit system and runs when `run=1` and needs at least 16 clock cycles produce the answer. The answer is 12 bit value.    
+
 
 ## How to test
 
-Explain how to use your project
+I've tested this using a verilator simulation included below - I like the `cpp` workbench for this. The testing has been mainly for numerical stability.
 
 ## External hardware
 
-List external hardware used in your project (e.g. PMOD, LED display, etc), if any
+I intend for this to be driven by the RP2040 and to work as a "coprocessor" for vector calculations
--- src/project.v
+++ src/project.v
@@ -6,28 +6,60 @@
 `define default_netname none
 
 module tt_um_fountaincoder_top (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+#()
+(	input wire clk,
+    input wire rst_n,     // reset_n - low to reset
+	input wire [3:0]  index,
+	input wire [3:0]  data,	
+	input wire [1:0]  insn,
+	input wire load,
+    input wire run,
+    output [12:0] out 	
 );
+ 	
+	reg [3:0] i = 1;
+	reg [3:0] i_e = 4'b1111;  	
+	reg signed [3:0] i_d= 1;
+	reg signed [5:0] mem[15:0];
+	 
+	reg hit  = 0;
+	reg halt = 0;
+	reg signed [5:0] delta;
+	reg [8:0] count;
+	reg [10:0] total;
+	reg [12:0] out_i;	
+	always @(posedge clk) begin
+	  	casez ({rst_n,run,load,insn,hit,halt})
 
-DMADD madd(
-	  	.clk    (clk),
-	  	.ena 	(ena),
-	  	.run    (ui_in[3]),
-	  	.load   (ui_in[2]),
-	  	.insn   (ui_in[1:0]),
-		.index  (uio_in[7:4]),
-		.data   (uio_in[3:0]),
-		.out    (uo_out),
-		.out_i  (uo_out2),
-		.rst_n  (rst_n)
+			//Reset
+     		7'b0_?_?_??_?_?: {i,i_e,hit,halt} <= {4'b0,4'b1111,1'b0,1'b0}; 
 
-);
+     		//Initialise
+     		7'b1_0_0_00_0_0: {i,i_e}          <= {4'b0,4'b1111}; 		//MIN
+     		7'b1_0_0_01_0_0, 											//MAX										
+     		7'b1_0_0_10_0_0: {i,i_e,i_d}      <= {4'b1111,4'b0,-4'b1};	//MADD
+     		
+	  		//Load Data
+     		7'b1_0_1_00_0_0,										//MIN
+     		7'b1_0_1_01_0_0: mem[index] <= 6'b1;					//MAX														 	
+     		7'b1_0_1_10_0_0: {mem[index], mem[index-1]} <= {mem[index] + {2'b0,data}, mem[index-1] - {2'b0,data}} ; //MADD
+  
+     		//Update and Halt
+     		7'b1_1_0_00_0_0, 				//MIN
+     		7'b1_1_0_01_0_0: i <= i + 1;	//MAX														 	
+     		7'b1_1_0_00_1_0,				//MIN
+     		7'b1_1_0_01_1_0: halt <= 1; 	//MAX	
+     		7'b1_1_0_10_?_0: {i,delta,count,total} <= {i  + i_d, delta + mem[i], count+{3'b0,delta}, total + {2'b0,count}} ; //MADD
+     		
+			default: halt <= 1;
+         endcase
+
+		if (i == i_e) begin
+			halt <= 1;
+		end
+	end
+	
+	assign hit      = mem[i] != 6'b0; 
+	assign out      = insn[1]  ? {2'b0,total} + {4'b0,count} : {9'b0,i} ;
 
 endmodule
